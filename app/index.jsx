3e3c2e22d2f2fa49a80e70d9a9ae75479b885b9a
CamilaRaimundo <camilaapereira.r@gmail.com>
2025-09-06 17:08:10 -0300

    Criação das telas e começando suas funcionalidades principais.

--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,110 @@
-import { Background } from "@react-navigation/elements";
-import { Text, View, StyleSheet } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { Text, View, StyleSheet, Image, Pressable } from "react-native";
+import { FokusButton } from "../components/FokusButton"
+
+
+const pomodoro = [
+  {
+    id: 'focus',
+    initialValue: 25,
+    image: require('../assets/images/pomodoro.png'),
+    display: 'Foco'
+  },
+  {
+    id: 'short',
+    initialValue: 5,
+    image: require('../assets/images/short.png'),
+    display: 'Pausa curta'
+  },
+  {
+    id: 'long',
+    initialValue: 15,
+    image: require('../assets/images/long.png'),
+    display: 'Pausa longa'
+  }
+]
 
 export default function Index() {
+
+  const [timerType, setTimerType] = useState(pomodoro[0])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Image source={timerType.image}/>
+      <View style={styles.actions}>
+        <View style={styles.context}>
+          {pomodoro.map(p => (
+            <Pressable 
+              key={p.id}
+              style={ timerType.id === p.id ? style.contextButtonActive : null }
+              onPress={() => setTimerType(p)}
+            >
+              <Text style={styles.contextButtonText}>{p.display}</Text>
+            </Pressable>
+          ))} 
+        </View>
+        <Text style={styles.timer}>
+          { new Date(timerType.initialValue * 1000).toLocaleTimeString('pt-BR', { minute: '2-digit', second: '2-digit' }) }
+        </Text>
+        <FokusButton/>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Projeto desensenvolvido no curso da Alura</Text>
+        <Text style={styles.footerText}>Camila Pereira</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet({
+const styles = StyleSheet.create({
   container:{
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgrounColor: "#021123"
+    backgroundColor: "#021123",
+    gap: 40,
   },
-  text: {
-    color: "#FFF",
-  }
 
+  actions: {
+    padding: 24,
+    backgroundColor:"#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32
+  },
+
+  context: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignContent: "center",
+  },
+
+  contextButtonText: {
+    fontSize: 12.5,
+    color: "#FFF",
+    padding: 8,
+  },
+
+    contextButtonActive: {
+    backgroundColor: "#144480",
+    borderRadius: 8,
+  },
+
+  timer: {
+    fontSize: 54,
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  footer: {
+    width: "80%"
+  },
+
+  footerText: {
+  textAlign: "center",
+  color: "#98A0A8",
+  fontSize: 12.5
+  }
 })
