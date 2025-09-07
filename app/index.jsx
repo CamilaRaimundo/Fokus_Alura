75c382575926036e2d545130b13d8eb083c20e4a
CamilaRaimundo <camilaapereira.r@gmail.com>
2025-09-07 20:52:11 -0300

    Criando o decremento do timer e setando os seus estados.

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
 
 import { FokusButton } from "../components/FokusButton"
@@ -30,6 +30,44 @@ const pomodoro = [
 export default function Index() {
 
   const [timerType, setTimerType] = useState(pomodoro[0])
+  const [timerRunning, setTimerRunning] = useState(false)
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
+
+  const timerRef = useRef(null)
+
+  const clear = () => {
+    if (timerRef.current != null) {
+      clearInterval(timerRef.current)
+      timerRef.current = null
+      setTimerRunning(false)
+    }
+  }
+
+  const toggleTimerType = (newTimerType) => {
+    setTimerType(newTimerType)
+    setSeconds(newTimerType.initialValue)
+    clear()
+  }
+
+  const toggleTimer = () => {
+    if (timerRef.current) {
+      clear()
+      return
+    }
+    setTimerRunning(true)
+
+    const id = setInterval(() => {
+      setSeconds(oldState => {
+        if (oldState === 0) {
+          clear()
+          return timerType.initialValue
+        }
+        return oldState - 1
+      })
+      console.log('tic')
+    }, 1000)
+    timerRef.current = id
+  }
 
   return (
     <View style={styles.container}>
@@ -40,13 +78,15 @@ export default function Index() {
             <ActionButton
               key={p.id}
               active={timerType.id === p.id}
-              onPress={() => setTimerType(p)}
+              onPress={() => toggleTimerType(p)}   // usa sua função que já reseta
               display={p.display}
             />
           ))}
         </View>
-        <Timer totalSeconds={timerType.initialValue} />
-        <FokusButton />
+        <Timer totalSeconds={seconds} />
+        <FokusButton
+          title={timerRunning ? 'Pausar' : 'Começar'}
+          onPress={toggleTimer} />
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>Projeto desensenvolvido no curso da Alura</Text>
